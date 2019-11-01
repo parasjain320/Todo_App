2ef2343e4ce231e81c0285496da5dd83c7644be0
paras jain <32354196+parasjain320@users.noreply.github.com>
2019-11-01 16:23:50 +0530

    Add files via upload

---
+++ main/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns  = [
+    path('', views.home, name = 'home'),
+    path('add_todo/', views.add_todo, name='index'),
+    path('delete_todo/<int:todo_id>', views.delete_todo),
+    ]
